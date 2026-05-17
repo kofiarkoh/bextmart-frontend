73bc6cefa3f970c22e2dd250acbe5fb9e042987b
kofiarkoh <kofiarkoh@gmail.com>
2026-05-17 18:07:08 -0400

    remove product discount

--- components/ultils/ProductLabels.js
+++ components/ultils/ProductLabels.js
@@ -5,9 +5,6 @@ const ProductLabels = ({ product }) => {
     const { t } = useTranslation();
     return (
         <div className="product-labels product-item__label">
-            <div className="product__labels-item product__labels-sale">
-                {showDiscount(product.price, product.price_compare)}%
-            </div>
             {hotlabel(product.label_hot, t("hot"))}
             {newlabel(product.label_new, t("new"))}
             {featuredlabel(product.label_featured, t("featured"))}
