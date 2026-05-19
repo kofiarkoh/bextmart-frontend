5dbc99fc05f019c4eb37c3e9364776cdee5142ec
kofiarkoh <kofiarkoh@gmail.com>
2026-05-19 08:32:18 -0400

    add product sharing

--- pages/product/[pid].js
+++ pages/product/[pid].js
@@ -441,24 +441,21 @@ const ProductPage = () => {
                                                             <div className={`product-sharing__title ${styles.details_strong}`}>{t("Share")}</div>
                                                             <ul className="social-sharing">
                                                                 <li>
-                                                                    <Link target="_blank" href={`https://www.facebook.com/sharer.php?u=${FullURL}`} className="btn--share share-facebook">
-                                                                        <SVGTwitter />
+                                                                    <Link target="_blank" href={`https://www.facebook.com/sharer.php?u=${encodeURIComponent(FullURL)}`} className="btn--share share-facebook">
+                                                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
                                                                         <span className="share-title" aria-hidden="true">Facebook</span>
-                                                                        <span className="visually-hidden">Facebook</span>
                                                                     </Link>
                                                                 </li>
                                                                 <li>
-                                                                    <Link target="_blank" href={`https://twitter.com/share?text=Duis%20sagittis%20porta&amp;url=${FullURL}`} className="btn--share share-twitter">
-                                                                        <SVGFacebook />
+                                                                    <Link target="_blank" href={`https://api.whatsapp.com/send?text=${encodeURIComponent(product.name + ' ' + FullURL)}`} className="btn--share share-whatsapp">
+                                                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 0 1-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 0 1-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 0 1 2.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0 0 12.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 0 0 5.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 0 0-3.48-8.413z"/></svg>
+                                                                        <span className="share-title" aria-hidden="true">WhatsApp</span>
+                                                                    </Link>
+                                                                </li>
+                                                                <li>
+                                                                    <Link target="_blank" href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(product.name)}&url=${encodeURIComponent(FullURL)}`} className="btn--share share-twitter">
+                                                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.737-8.835L1.254 2.25H8.08l4.259 5.63zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
                                                                         <span className="share-title" aria-hidden="true">Twitter</span>
-                                                                        <span className="visually-hidden">Twitter</span>
-                                                                    </Link>
-                                                                </li>
-                                                                <li>
-                                                                    <Link target="_blank" href={`https://pinterest.com/pin/create/button/?url=${FullURL}`} className="btn--share share-pinterest">
-                                                                        <SVGPinterest />
-                                                                        <span className="share-title" aria-hidden="true">Pinterest</span>
-                                                                        <span className="visually-hidden">Pinterest</span>
                                                                     </Link>
                                                                 </li>
                                                             </ul>
