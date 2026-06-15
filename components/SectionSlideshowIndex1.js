1e0bbf74f624692df93bf64f2b8955b48fe18bb9
kofiarkoh <kofiarkoh@gmail.com>
2026-06-15 16:21:46 -0400

    clean up banners

--- components/SectionSlideshowIndex1.js
+++ components/SectionSlideshowIndex1.js
@@ -1,15 +1,12 @@
 
 import React from "react";
 import Link from 'next/link';
-import Image from 'next/image'
 import useTranslation from './ultils/useTranslation'
-import AllCategories from './ultils/LeftAllCategories'
 import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 import 'swiper/css';
-import {DataIndexSlideshow} from './data/DataIndexSlideshow';
 import { SVGArrowLeft, SVGArrowRight } from '../public/assets/SVG';
 import styles from '../public/assets/styles/Home.module.css'
 import { useGetBannersQuery } from '../store/productsApi';
@@ -67,7 +64,6 @@ const SectionSlideshowIndex1 = () => {
     const published = bannersData?.data?.filter(b => b.status === 'published') || [];
     const sliderBanners = published.filter(b => b.type === 'slider');
     const overlayBanners = published.filter(b => b.type === 'overlay');
-    const fallbackSlides = DataIndexSlideshow();
 
     return (
         <>
@@ -78,77 +74,41 @@ const SectionSlideshowIndex1 = () => {
                             <div className={styles.slideshow_component}>
                                 <div className={`${styles.slideshow_container} slideshow-template`}>
                                     <Swiper {...carouselOptions} className={`${styles.slideshow_container_swiper_container} swiper-container`}>
-                                        {sliderBanners.length > 0 ? sliderBanners.map((banner) => (
+                                        {sliderBanners.map((banner) => (
                                             <SwiperSlide key={banner.id} className={styles.slideshow_container_swiper_slide}>
                                                 <div className={styles.slideshow_slide_background}>
                                                     <div
                                                         className={styles.slideshow_slide__background}
-                                                        style={{ height: 0, backgroundImage: `url(${buildImageUrl(banner.file)})`, paddingBottom: '31.25%' }}
+                                                        style={{ height: 0, backgroundImage: `url(${buildImageUrl(banner.file)})`,
+                                                        paddingBottom: '31.25%' ,
+                                                        backgroundSize: 'contain',
+                                                        backgroundRepeat: 'no-repeat',}}
                                                     />
                                                 </div>
                                             </SwiperSlide>
-                                        )) : (
-                                            <>
-                                                <SwiperSlide className={styles.slideshow_container_swiper_slide}>
-                                                    <div className={styles.slideshow_slide_background}>
-                                                        <div className={styles.slideshow_slide__background} style={{ height: 0, backgroundImage: `url(${fallbackSlides.slideshow1})`, paddingBottom: '31.25%' }} />
-                                                    </div>
-                                                </SwiperSlide>
-                                                <SwiperSlide>
-                                                    <div className={styles.slideshow_slide_background}>
-                                                        <div className={styles.slideshow_slide__background} style={{ height: 0, backgroundImage: `url(${fallbackSlides.slideshow2})`, paddingBottom: '31.25%' }} />
-                                                    </div>
-                                                </SwiperSlide>
-                                            </>
-                                        )}
+                                        ))}
                                     </Swiper>
                                     <div className="index-slideshow-pagination"></div>
                                     <div className="carousel-navigation tops-carousel-nav-prev swiper-nav-prev"><SVGArrowLeft /></div>
                                     <div className="carousel-navigation tops-carousel-nav-next swiper-nav-next"><SVGArrowRight /></div>
                                 </div>
                             </div>
-                        </div>
-                        <div className={styles.slideshow_ontop}>
-                            <div className="container">
-                                <div className={`${styles.slideshow_ontop_row1 } row`}>
-                                    <div className={`${styles.slideshow_ontop_row1_allcollections } col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 col-xxl-2 d-none d-lg-block`}>
-                                        <div className="main-header__allcollections">
-                                            <div className="top-header__menu-root header__allcollections">
-                                                <div className="header__categorie-dropdown">
-                                                    <AllCategories />
+                            <div className={styles.slideshow_bottom_overlay}>
+                                <div className="container">
+                                    <div className={`${styles.slideshow_ontop_row2} row`}>
+                                        {overlayBanners.slice(0, 3).map((banner, i) => {
+                                            const href = banner.category?.slug ? `/products?category=${banner.category.slug}` : null;
+                                            const img = <img src={buildImageUrl(banner.file)} alt={banner.category?.name || ''} style={{ width: '100%', height: 'auto', objectFit: 'cover', display: 'block' }} />;
+                                            return (
+                                                <div key={banner.id} className={`slideshow-ontop-bbanner slideshow-ontop-bbanner-${i + 1} effect-shine effect col-12 col-md-6 col-lg-3`}>
+                                                    {href ? (
+                                                        <Link href={href} className="effect-parent">{img}</Link>
+                                                    ) : (
+                                                        <div className="effect-parent">{img}</div>
+                                                    )}
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 col-xxl-2">
-                                        <div className={styles.slideshow_ontop_row1_rbanners}>
-                                            {overlayBanners.length > 0 ? overlayBanners.slice(0, 2).map((banner, i) => {
-                                                const href = banner.category?.slug ? `/products?category=${banner.category.slug}` : null;
-                                                const img = <img src={buildImageUrl(banner.file)} alt={banner.category?.name || ''} style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block' }} />;
-                                                return (
-                                                    <div key={banner.id} className={`slideshow-ontop-rbanner slideshow-ontop-rbanner-${i + 1} effect-shine effect`}>
-                                                        {href ? (
-                                                            <Link href={href} className="effect-parent">{img}</Link>
-                                                        ) : (
-                                                            <div className="effect-parent">{img}</div>
-                                                        )}
-                                                    </div>
-                                                );
-                                            }) : (
-                                                <>
-                                                    <div className="slideshow-ontop-rbanner slideshow-ontop-rbanner-1 effect-shine effect">
-                                                        <Link href={fallbackSlides.img1_link} className="effect-parent">
-                                                            <Image src={fallbackSlides.img1} alt="" width={250} height={225} />
-                                                        </Link>
-                                                    </div>
-                                                    <div className="slideshow-ontop-rbanner slideshow-ontop-rbanner-2 effect-shine effect">
-                                                        <Link href={fallbackSlides.img2_link} className="effect-parent">
-                                                            <Image src={fallbackSlides.img2} alt="" width={250} height={225} />
-                                                        </Link>
-                                                    </div>
-                                                </>
-                                            )}
-                                        </div>
+                                            );
+                                        })}
                                     </div>
                                 </div>
                             </div>
--- components/ultils/HorizontalMenu.js
+++ components/ultils/HorizontalMenu.js
@@ -4,13 +4,16 @@ import { useRouter } from "next/router";
 import MenuSub from './MenuSub'
 import useTranslation from './useTranslation'
 import ProductItemList from './ProductItemList'
-import { SVGArrowDown } from '../../public/assets/SVG';
+import { SVGArrowDown, SVGArrowRight } from '../../public/assets/SVG';
 import {HorizontalData} from '../data/DataHeader'
 import MenuBKG from '../../public/assets/images/MenuBKG.png'
+import { useGetCategoriesQuery } from '../../store/productsApi';
 
 const HorizontalMenu = () => {
     const { t } = useTranslation();
     const router = useRouter();
+    const { data: categoriesData, isLoading: isCategoriesLoading } = useGetCategoriesQuery();
+    const categories = categoriesData?.data?.data?.slice(0, 10) || [];
     let TextMenu = HorizontalData().TextMenu,
     TextMenuCol1 = HorizontalData().TextMenuCol1,
     TextMenuCol2 = HorizontalData().TextMenuCol2,
@@ -29,6 +32,35 @@ const HorizontalMenu = () => {
                         <Link href={TextMenu.find((m) => m.id === 1).url} className={`header__menu-item list-menu__item ${ (router.asPath === TextMenu.find((m) => m.id === 1).url)? 'header__menu-active':'' }`}>
                             {TextMenu.find((m) => m.id === 1).text}
                         </Link>
+                    </li>
+                    <li className="header__menu-root menu__dropdown header__categories-menu">
+                        <div className="header__menu-item list-menu__item dropdown-toggle">
+                            <span className="text">Categories</span>
+                        </div>
+                        <div className="header__submenu dropdown-menu">
+                            <nav className="header__vertical-menu">
+                                <ul className="list-menu list-menu--vertical" role="list">
+                                    {isCategoriesLoading && (
+                                        <li style={{ padding: '12px 16px', fontSize: 13, color: '#888' }}>Loading...</li>
+                                    )}
+                                    {categories.map((cat) => (
+                                        <li key={cat.id} className="header__vertical-root">
+                                            <Link
+                                                className="header__vertical-item list-menu__item"
+                                                href={`/products?category=${cat.slug}`}
+                                                style={{ display: 'flex', alignItems: 'center', gap: 10 }}
+                                            >
+                                                <span className="header__vertical-icon" style={{ display: 'flex', alignItems: 'center', width: 20 }}>
+                                                    <i className={`ph ${cat.icon}`} style={{ fontSize: 18 }} />
+                                                </span>
+                                                <span className="text">{cat.name}</span>
+                                                <SVGArrowRight />
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </nav>
+                        </div>
                     </li>
                     <li className="header__menu-root menu__dropdown menu__mega">
                         <Link href={TextMenu.find((m) => m.id === 2).url} className={`header__menu-item list-menu__item dropdown-toggle ${ (router.asPath === TextMenu.find((m) => m.id === 2).url)? 'header__menu-active':'' }`}>
